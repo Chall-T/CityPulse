e869e9e73b992aad63f5a259ec0239692fedffa3
Chall-T <gdav476@gmail.com>
2025-09-15 11:55:02 +0200

    loginpage correction for mobile

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -73,8 +73,8 @@ export const RegistrationPage = () => {
   };
 
   return (
-    <div className="max-w-xl mt-7 bg-white border border-gray-200 rounded-xl shadow-2xs dark:bg-neutral-900 dark:border-neutral-700 w-1/2 mx-auto bg-gray-200 p-4">
-      <div className="p-4 sm:p-7">
+    <div className="min-h-screen flex items-center justify-center dark:bg-neutral-900 px-4">
+      <div className="w-full max-w-xl sm:w-3/4 md:w-2/3 lg:w-1/2 bg-white dark:bg-neutral-900 border border-gray-200 dark:border-neutral-700 rounded-xl shadow-2xs p-6 sm:p-7">
         <div className="text-center">
           <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">Sign up</h1>
           <p className="mt-2 text-sm text-gray-600 dark:text-neutral-400">
